fb60553a382b45226d70316f3c87ffcfd21ce465
Damian Demasi <damian.demasi.1@gmail.com>
2021-08-18 10:15:21 +0930

    Basic component built

--- 01-my-react-files/01-project/my-react-project-1/src/App.js
+++ 01-my-react-files/01-project/my-react-project-1/src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible! 😅</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ 01-my-react-files/01-project/my-react-project-1/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amount</div>
+            </div>
+        </div>);
+}
+
+export default ExpenseItem;
