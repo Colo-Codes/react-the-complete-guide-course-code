522c10b6e768bd62981b7bbf5dc8c7b599608d7e
Damian Demasi <damian.demasi.1@gmail.com>
2021-08-19 09:15:50 +0930

    React state events

---
+++ 01-my-react-files/01-project/my-react-project-1/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NewExpense = () => {
+
+    return (
+        <div>
+            <form>
+
+            </form>
+        </div>
+    );
+}
+
+export default NewExpense;
