31827a238dbf2074150ebce76f5bc115fae4f206
Damian Demasi <damian.demasi.1@gmail.com>
2021-08-23 09:36:14 +0930

    Using styled-components to limit scope of styles

--- 01-my-react-files/02-project-styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-my-react-files/02-project-styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,13 +25,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+      <div className={`form-control ${!isValid && 'invalid'}`}>
+        <label>Course Goal</label>
         <input
-          style={{
-            borderColor: !isValid ? 'red' : '#ccc',
-            background: !isValid ? 'salmon' : 'transparent'
-          }}
           type="text"
           onChange={goalInputChangeHandler}
         />
