26ac41092d11a27943f41f74c4c0504155af65a0
Damian Demasi <damian.demasi.1@gmail.com>
2021-08-23 10:23:23 +0930

    Using styled-components with dynamic styles and media queries

--- 01-my-react-files/02-project-styling/src/components/UI/Button/Button.js
+++ 01-my-react-files/02-project-styling/src/components/UI/Button/Button.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 // import './Button.css';
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,6 +12,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+@media (min-width: 768px) {
+  width: auto;
+}
 
 &:focus {
   outline: none;
