c460f1741265496e327446948835360cfe30606d
Damian Demasi <damian.demasi.1@gmail.com>
2021-08-26 15:56:19 +0930

    useEffect

--- 01-my-react-files/08-side-effects-reducers-context/src/App.js
+++ 01-my-react-files/08-side-effects-reducers-context/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,31 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+
+  // Just running the setIsLoggedIn will cause a loop, because it will cause the page to re-render and get the value again and again.
+  // This is why we use an Effect.
+  // const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+  // if (storedUserLoggedInInformation === '1') {
+  //   setIsLoggedIn(true);
+  // }
+
+  // This function is executed after every component re-evaluation, if the dependencies defined in the second argument have changed, or will run just once, at start, if dependencies didn't change.
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
