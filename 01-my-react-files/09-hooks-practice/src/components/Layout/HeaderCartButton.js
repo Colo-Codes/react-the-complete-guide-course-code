8b5754fef7de9fca9a0e7440b0ad6d19b6266271
Damian Demasi <damian.demasi.1@gmail.com>
2021-08-31 11:34:35 +0930

    Playing animations as side effects

--- 01-my-react-files/09-hooks-practice/src/components/Layout/HeaderCartButton.js
+++ 01-my-react-files/09-hooks-practice/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,40 @@
 import classes from './HeaderCartButton.module.css';
 
 import CartIcon from '../Cart/CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CardContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     // NOTE: The context is managed by the closest provider.
     // NOTE: Whenever the context changes, this component will be re-evaluated by React.
     const cartCtx = useContext(CardContext);
 
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => curNumber + item.amount, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        // Remove the class so it can be added on new item addition
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        // Use the cleaner function in case the component is removed (good practice on all side effects)
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [cartCtx.items]);
+
     return (
-        <button className={classes.button} onClick={props.myOnClick}>
+        <button className={btnClasses} onClick={props.myOnClick}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={classes.badge}>{numberOfCartItems}</span>
