9fc847ccf1e7a00ec45203f86ac6ee6b296a386d
Damian Demasi <damian.demasi.1@gmail.com>
2021-09-01 12:24:44 +0930

    useCallback

--- 01-my-react-files/10-how-react-works/src/App.js
+++ 01-my-react-files/10-how-react-works/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
@@ -6,21 +6,29 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('(PARENT) App re-evaluation');
 
-  const clickHandler = () => {
-    setShowParagraph(prevState => !prevState);
+  const clickHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph(prevState => !prevState);
+    }
+  }, [allowToggle]);
+
+  const clickAllowToggleHandler = () => {
+    setAllowToggle(prevState => !prevState);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <Button onClick={clickHandler}>Toggle paragraph</Button>
+      <Button onClick={clickAllowToggleHandler}>Allow toggling</Button>
+      {allowToggle && <Button onClick={clickHandler}>Toggle paragraph</Button>}
       {/* The child component will always re-evaluate when the parent's state changes */}
-      {/* <DemoOutput show={showParagraph} /> */}
+      <DemoOutput show={showParagraph} />
       {/* But the child component will only trigger a re-rendering if an HTML element changes (not in this case, as it is always 'false') */}
-      <DemoOutput show={false} />
+      {/* <DemoOutput show={false} /> */}
     </div>
   );
 }
