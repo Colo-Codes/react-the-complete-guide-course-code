129e3d8d55d87d2f878671029bd9f2e3e689891c
Damian Demasi <damian.demasi.1@gmail.com>
2021-09-02 14:13:42 +0930

    Handling HTTP errors

--- 01-my-react-files/12-sending-http-requests/src/App.js
+++ 01-my-react-files/12-sending-http-requests/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   // // Using fetch() method from the Fetch API to send a GET request to an external API (Star Wars API)
   // function fetchMoviesHandler() {
@@ -29,22 +30,48 @@ function App() {
   // Using async...await
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    // fetch() sends a GET request by default
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl
+    setError(null);
+    try {
+      // fetch() sends a GET request by default
+      // const response = await fetch('https://swapi.dev/api/films/');
+      // const response = await fetch('https://swapi.dev/api/films/ERROR');
+      const response = await fetch('https://swapi.dev/api/filmsERROR');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
       }
-    });
-    // Trigger a state update with the new data
-    setMovies(transformedMovies);
+
+      const data = await response.json();
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl
+        }
+      });
+      // Trigger a state update with the new data
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   }
 
+  // Handling messages to user
+  let content = <p>Found no movies! <span role="img" aria-label="Ho no!">🤭</span></p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error} <span role="img" aria-label="Ho no!">😱</span></p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading movies... <span role="img" aria-label="Loading">🍿</span></p>;
+  }
 
   return (
     <React.Fragment>
@@ -52,9 +79,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies! <span role="img" aria-label="Ho no!">🤭</span></p>}
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies! <span role="img" aria-label="Ho no!">🤭</span></p>}
         {isLoading && <p>Loading movies...</p>}
+        {!isLoading && error && <p>{error}</p>} */}
+        {content}
       </section>
     </React.Fragment>
   );
