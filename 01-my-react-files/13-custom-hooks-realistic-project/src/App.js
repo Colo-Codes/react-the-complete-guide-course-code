c3b1df8dd733200e2edcdf5fcfab28ff289b53c1
Damian Demasi <damian.demasi.1@gmail.com>
2021-09-03 14:06:47 +0930

    Creating a custom hook with functions as a link to external functions

--- 01-my-react-files/13-custom-hooks-realistic-project/src/App.js
+++ 01-my-react-files/13-custom-hooks-realistic-project/src/App.js
@@ -2,42 +2,56 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
-      );
+  const httpData = useHttp();
+  const { isLoading, error, sendRequest: fetchTasks } = httpData;
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
+  //     );
 
-      const data = await response.json();
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
 
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    const transformTasks = tasksObj => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({ url: 'https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json' }, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- 01-my-react-files/13-custom-hooks-realistic-project/src/components/NewTask/NewTask.js
+++ 01-my-react-files/13-custom-hooks-realistic-project/src/components/NewTask/NewTask.js
@@ -1,41 +1,63 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useEffect } from 'react';
+
+import useHttp from '../../hooks/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+    sendTaskRequest({
+      url: 'https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+      method: 'POST',
+      body: { text: taskText },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }, createTask.bind(null, taskText));
 
-      const data = await response.json();
+    //   setIsLoading(true);
+    //   setError(null);
+    //   try {
+    //     const response = await fetch(
+    //       'https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+    //       {
+    //         method: 'POST',
+    //         body: JSON.stringify({ text: taskText }),
+    //         headers: {
+    //           'Content-Type': 'application/json',
+    //         },
+    //       }
+    //     );
 
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
+    //     if (!response.ok) {
+    //       throw new Error('Request failed!');
+    //     }
 
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    //     const data = await response.json();
+
+    //     const generatedId = data.name; // firebase-specific => "name" contains generated id
+    //     const createdTask = { id: generatedId, text: taskText };
+
+    //     props.onAddTask(createdTask);
+    //   } catch (err) {
+    //     setError(err.message || 'Something went wrong!');
+    //   }
+    //   setIsLoading(false);
+
   };
 
   return (
