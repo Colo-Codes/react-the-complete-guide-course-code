69b6df7c4c10408cff45ec556d2137be4669fc7b
Damian Demasi <damian.demasi.1@gmail.com>
2021-09-06 10:49:33 +0930

    Getting data by using GET request

--- 01-my-react-files/15-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
+++ 01-my-react-files/15-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,36 @@
+import { useEffect, useState } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          // name: data[key].name,
+          // description: data[key].description,
+          // price: data[key].price,
+          ...data[key]
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
