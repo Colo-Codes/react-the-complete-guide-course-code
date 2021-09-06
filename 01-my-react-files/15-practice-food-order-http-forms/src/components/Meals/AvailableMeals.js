f1110804b87e07c445aba0ba07088813107c9ced
Damian Demasi <damian.demasi.1@gmail.com>
2021-09-06 11:43:34 +0930

    Managing errors

--- 01-my-react-files/15-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
+++ 01-my-react-files/15-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
@@ -4,12 +4,25 @@ import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
+
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isErrorPresent, setIsErrorPresent] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
+      // Toggle spinner
+      setIsLoading(true);
+
+      // const response = await fetch('https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/meals.ERROR');
       const response = await fetch('https://react-http-test-d8033-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+
+      // Error checking
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const data = await response.json();
 
       const loadedMeals = [];
@@ -24,11 +37,37 @@ const AvailableMeals = () => {
         });
       }
 
+      // Toggle spinner
+      setIsLoading(false);
+
       setMeals(loadedMeals);
     };
-    fetchMeals();
+
+    // Using catch directly on fetchMeals() function because it's a promise
+    fetchMeals().catch(error => {
+      // Error handling
+      setIsLoading(false);
+      setIsErrorPresent(error.message);
+
+    });
   }, []);
 
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading meals...</p>
+      </section>
+    );
+  }
+
+  if (isErrorPresent) {
+    return (
+      <section className={classes.mealsError}>
+        <p>🚨 ERROR!</p>
+        <p>{isErrorPresent}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
