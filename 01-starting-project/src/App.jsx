58eebb822c13660a6fb2857a42ff988e3af58987
Damian Demasi <damian.demasi.1@gmail.com>
2024-07-31 19:42:15 +0930

    Conditional rendering

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,19 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 // Header component
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
