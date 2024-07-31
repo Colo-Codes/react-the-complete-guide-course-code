e58cc44402dcedd2a8045a0bbc340a800f0aa1f5
Damian Demasi <damian.demasi.1@gmail.com>
2024-07-31 18:33:33 +0930

    1st component exercise

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,23 @@
+// Header component
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+// Main component
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      {/* We use the header component as if it were an HTML element */}
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
