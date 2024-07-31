ab513d0207c86f2b9c41da2443d423567f4eecb5
Damian Demasi <damian.demasi.1@gmail.com>
2024-08-01 07:52:09 +0930

    Use props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import headerImage from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,14 +23,46 @@ function Header() {
   );
 }
 
+// CoreConcept component
+function CoreConcept({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt="..." />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 // Main component
 function App() {
+  let coreConcepts = [];
+
+  // Advanced way of creating multiple components based on iterable data (an array in this case)
+  CORE_CONCEPTS.forEach((concept) => {
+    coreConcepts.push(
+      // Using a naive approach:
+      // <CoreConcept
+      //   key={concept.title}
+      //   title={concept.title}
+      //   description={concept.description}
+      //   image={concept.image}
+      // />
+
+      // Using an optimal approach (this works because the object keys have the same name as the components props):
+      <CoreConcept {...concept} />
+    );
+  });
+
   return (
     <div>
       {/* We use the header component as if it were an HTML element */}
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>{coreConcepts}</ul>
+        </section>
       </main>
     </div>
   );
