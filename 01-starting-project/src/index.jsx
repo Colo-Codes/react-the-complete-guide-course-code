fb1ff880fc30999ed7799087c32abaded1de5552
Damian Demasi <damian.demasi.1@gmail.com>
2024-08-05 07:08:59 +0930

    No JSX

--- 01-starting-project/src/index.jsx
+++ 01-starting-project/src/index.jsx
@@ -1,7 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+// ReactDOM.createRoot(entryPoint).render(<App />);
+// Using React.createElement here to prove the point that we can use React without using JSX
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
