7b3b3978c91ae5de62075714e0cd9c15ecff7c7d
Damian Demasi <damian.demasi.1@gmail.com>
2024-08-12 07:35:22 +0930

    CSS Modules and debugging, initial files

--- 04-styling-project/src/components/Header.jsx
+++ 04-styling-project/src/components/Header.jsx
@@ -1,11 +1,12 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={styles.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
