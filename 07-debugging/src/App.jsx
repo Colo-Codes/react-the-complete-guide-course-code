03d18a5c8093ac24ab7b7600a3c16cfa5ecce3f9
Damian Demasi <damian.demasi.1@gmail.com>
2024-08-13 08:05:18 +0930

    Debugging, project structure organization, TypeScript initial files

--- 07-debugging/src/App.jsx
+++ 07-debugging/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Header.jsx';
-import UserInput from './components/UserInput.jsx';
-import Results from './components/Results.jsx';
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -16,7 +16,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
--- 07-debugging/src/components/Results.jsx
+++ 07-debugging/src/components/Results.jsx
@@ -1,9 +1,14 @@
-import { calculateInvestmentResults, formatter } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
+const results = [];
 
 export default function Results({ input }) {
-  const results = [];
   calculateInvestmentResults(input, results);
+
+  if (results.length === 0) {
+    return <p className="center">Invalid input data provided.</p>;
+  }
+
   const initialInvestment =
     results[0].valueEndOfYear -
     results[0].interest -
