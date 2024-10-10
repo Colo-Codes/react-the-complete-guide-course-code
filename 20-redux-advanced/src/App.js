70016a52f3a872464c837e4729f560031b150ae4
Damian Demasi <damian.demasi.1@gmail.com>
2024-10-11 07:37:14 +1030

    Send cart to Firebase database

--- 20-redux-advanced/src/App.js
+++ 20-redux-advanced/src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -6,6 +7,17 @@ import Products from "./components/Shop/Products";
 function App() {
   // Access to the state
   const isCartShown = useSelector((state) => state.ui.isCartShown);
+  const cart = useSelector((state) => state.cart.cartItems);
+
+  useEffect(() => {
+    fetch(
+      "https://react-course-backend-21ec7-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      },
+    );
+  }, [cart]);
 
   return (
     <Layout>
